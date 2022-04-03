8e67b112070e36a396c8281ca104851fe52b2e13
eduardevdokimov <eduard.evdokimov@inbox.ru>
2022-04-03 18:22:55 +0300

    update 03.04.2022

---
+++ src/Controllers/ConsultationController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controllers;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route(path: 'consultations', name: 'consultations.')]
+class ConsultationController extends AbstractController
+{
+    #[Route(path: '/', name: 'index')]
+    public function index(): Response
+    {
+        return $this->render('consultations/index.html.twig');
+    }
+
+    #[Route(path: '/{consultation<\D+>}', name: 'show')]
+    public function show(): Response
+    {
+        return $this->render('consultations/show.html.twig');
+    }
+}
